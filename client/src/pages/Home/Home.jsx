a42d13e814920ee72064e95f7d5d3d7345d48312
MuradMammadov1 <muradsma@code.edu.az>
2026-04-07 13:42:34 +0400

    added hero section

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import "./home.css";
 
 const Home = () => {
   return (
-    <div>
-      <h1>Home Page</h1>
-    </div>
+    <section className="hero">
+      <div className="hero-content">
+        <p className="hero-subtitle">MEN COLLECTION</p>
+
+        <h1 className="hero-title">
+          Show Your <br />
+          Personal <span>Style</span>
+        </h1>
+
+        <p className="hero-desc">
+          Fowl saw dry which a above together place.
+        </p>
+
+        <button className="hero-btn">VIEW COLLECTION</button>
+      </div>
+    </section>
   );
 };
 
