d4de1b3c902ec7c6500760bc6abd6f714d09f3a9
MuradMammadov1 <muradsma@code.edu.az>
2026-04-08 10:50:29 +0400

    complated featured products section

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,23 +1,29 @@
+import React from "react";
+import FeaturedProduct from "./FeaturedProduct";
 import "./home.css";
 
 const Home = () => {
   return (
-    <section className="hero">
-      <div className="hero-content">
-        <p className="hero-subtitle">MEN COLLECTION</p>
+    <>
+      <section className="hero">
+        <div className="hero-content">
+          <p className="hero-subtitle">MEN COLLECTION</p>
 
-        <h1 className="hero-title">
-          Show Your <br />
-          Personal <span>Style</span>
-        </h1>
+          <h1 className="hero-title">
+            Show Your <br />
+            Personal <span>Style</span>
+          </h1>
 
-        <p className="hero-desc">
-          Fowl saw dry which a above together place.
-        </p>
+          <p className="hero-desc">
+            Fowl saw dry which a above together place.
+          </p>
 
-        <button className="hero-btn">VIEW COLLECTION</button>
-      </div>
-    </section>
+          <button className="hero-btn">VIEW COLLECTION</button>
+        </div>
+      </section>
+
+      <FeaturedProduct />
+    </>
   );
 };
 
